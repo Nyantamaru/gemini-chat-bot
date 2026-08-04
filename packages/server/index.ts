f97197878c12080a96d3cf18edee39c83c58a32d
manuel <5506079+Nyantamaru@users.noreply.github.com>
2026-08-03 20:40:34 -0600

    Connect the Fronted and Backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -11,6 +11,10 @@ app.get('/', (req: Request, res: Response) => {
   res.send('HELLO BUN!');
 });
 
+app.get('/api/hello', (req: Request, res: Response) => {
+  res.json({ message: 'Hello, World! from Json Object' });
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
